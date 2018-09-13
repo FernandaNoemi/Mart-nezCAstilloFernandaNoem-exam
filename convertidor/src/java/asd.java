e2942f777abd9c1e51d63e8e1b82063912c6f9dc
FernandaNoemi <42645976+FernandaNoemi@users.noreply.github.com>
2018-09-13 08:55:54 -0500

    Add files via upload

---
+++ convertidor/src/java/asd.java
@@ -0,0 +1,41 @@
+
+import com.sun.xml.rpc.processor.modeler.j2ee.xml.string;
+
+
+public class asd {
+    int peso;
+    String cant;
+    String result;
+
+    public asd(int peso, String cant, String result) {
+        this.peso = peso;
+        this.cant = cant;
+        this.result = result;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getCant() {
+        return cant;
+    }
+
+    public void setCant(String cant) {
+        this.cant = cant;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+    
+    
+}
